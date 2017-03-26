f86d4ef0c09c0b3ef8671a6e252fc655b3775b36
trevormac <trevor.c.macgregor@gmail.com>
2017-03-26 14:31:59 -0400

    Initial Commit

---
+++ CollectionView Practice1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView Practice1
+//
+//  Created by Trevor MacGregor on 2017-03-26.
+//  Copyright © 2017 Trevor MacGregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionView Practice1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionView Practice1
+//
+//  Created by Trevor MacGregor on 2017-03-26.
+//  Copyright © 2017 Trevor MacGregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
